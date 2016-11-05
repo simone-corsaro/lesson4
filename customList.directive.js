62ca2e614169f50a79c4acb0c1d3de2392484f1d
simone-corsaro <simone.corsaro@hotmail.it>
2016-11-05 17:12:11 +0100

    Upload progetto finale Cosaro_Spoto
    Progetto "Simple task manager" di Simone Corsaro, Filippo Fabrizio Spoto

--- customList.directive.js
+++ customList.directive.js
@@ -18,11 +18,26 @@
             transclude: true,
             restrict: 'E',
             template: '' +
-                '            <md-content class="md-padding scroll tab-content">' +
+                '<md-content class="md-padding scroll tab-content">' +
+                '<md-content>'+
+                '<i class="material-icons">search</i>'+
+                '<md-input-container>'+
+                ' <input type="search" ng-model="customListCtrl.q" placeholder="search task..." aria-label="search task"/>'+
+                '</md-input-container>'+
+                ' <tr> '+
+                '   <th> '+
+                ' <b>Order by</b>   '+
+                '        <md-button class="btn1 md-raised md-primary" ng-click="customListCtrl.sortBy(\'title\')">Title</md-button> '+
+                '       <span class="sortorder" ng-show="customListCtrl.propertyName === \'title\' " ng-class="{reverse: customListCtrl.reverse}"></span>'+
+                '   </th> '+
+                ' </tr>'+
+                '    <th> '+
+                '        <md-button class="btn1 md-raised md-primary" ng-click="customListCtrl.sortBy(\'date\')">Date</md-button> '+
+                '        <span class="sortorder" ng-show="customListCtrl.propertyName === \'date\' " ng-class="{reverse: customListCtrl.reverse}"></span> '+
+                '    </th> '+
+                '</md-content>'+
                 '    <md-list>' +
-                '        <md-list-item class="md-2-line" ng-repeat="item in customListCtrl.items | filter: customListCtrl.filterFunction" ng-class="customListCtrl.selectedItem == item ? \'selected\':\'\'" ng-click="customListCtrl.toggleSelection(item)">'
-
-                +
+                '        <md-list-item class="md-2-line" ng-repeat="item in customListCtrl.items |  filter: customListCtrl.q | filter: customListCtrl.filterFunction | orderBy:customListCtrl.propertyName:customListCtrl.reverse" ng-class="customListCtrl.selectedItem[customListCtrl.multiSelection(item)]==item ? \'selected\':\'\'" ng-click="customListCtrl.toggleSelection(item)">'+
                 '            <md-button ng-click="customListCtrl.changePriority(item)" class="md-icon-button" aria-label="Priority">' +
                 '                <md-icon style="color: green" ng-if="item.priority == -1">low_priority</md-icon>' +
                 '                <md-icon ng-if="item.priority == 0">label</md-icon>' +
@@ -30,8 +45,23 @@
                 '            </md-button>' +
                 '            <div class="md-list-item-text">' +
                 '                <h3>{{item.title}}</h3>' +
-                '                <p> {{item.date | date: "dd-MM-yyyy HH:mm"}}</p>' +
+                '                <p>{{item.date | date: "MM-dd-yyyy"}}' +
+                '                    {{item.time | date: "HH:mm"}}</p>'+
+                '            <div layout="row">' +
+                '               <span ng-if="checked" class="animate-if">'+
+                '               <p><u>Description:</u> {{item.description}}</p>' +
+                '               <p><u>Priority:</u> {{item.priority}}</p>' +
+                '               <p><u>Tags:</u> {{item.tags}}</p>' +
+                '               <p><u>Estimated works:</u> {{item.estimated_work}}</p>' +
+                '               </span>'+
                 '            </div>' +
+                '            </div>' +
+                '           <i class="material-icons">remove_red_eye</i>'+
+                '           <md-checkbox ng-model="checked" ng-init="checked=false" />'+
+                '           </md-checkbox>'+
+                '           <md-button class="md-primary md-hue-1" aria-label="Modify" ng-click="customListCtrl.modifyTask(item)">'+
+                '               <md-icon>create</md-icon>'+
+                '           </md-button>'+
                 '            <md-checkbox ng-model="item.done" ng-change="customListCtrl.checkStateChanged()" class="md-primary md-align-top-right">' +
                 '            </md-checkbox>' +
                 '            <md-divider></md-divider>' +
@@ -41,8 +71,20 @@
         };
     }
     //Directive controller
-    function customListController(storageService) {
+    function customListController(storageService, $mdDialog, $scope) {
         var vm = this;
+
+        vm.propertyName = 'title';
+        vm.reverse = true;
+        vm.selectedItem = [];
+
+        
+        vm.sortBy = function(propertyName) {
+           vm.reverse = (vm.propertyName === propertyName) ? !vm.reverse : false;
+           vm.propertyName = propertyName;
+           vm.selectedItem = [];
+        
+        };
 
         //Changes the priority of the given item
         vm.changePriority = function(item) {
@@ -61,10 +103,90 @@
 
         //Select or deselect the given item
         vm.toggleSelection = function(item) {
-            if (vm.selectedItem == null || vm.selectedItem != item)
-                vm.selectedItem = item;
-            else
-                vm.selectedItem = null;
+            var temp=1;
+            var j=0;
+
+                while(j<vm.selectedItem.length){
+                    if(vm.selectedItem[j]==item){
+                        temp=0;
+                        vm.selectedItem.splice(j, 1);
+                    }
+                    j++;
+                }
+
+            if(temp==1){
+                 vm.selectedItem[vm.selectedItem.length] = item;
+            }
         }
+
+        vm.multiSelection = function(item){
+            var i=0;
+            while(i<vm.selectedItem.length){
+                if(vm.selectedItem[i] == item)
+                    return i;
+                i++;
+            }
+        }
+
+        vm.modifyTask = function(item){
+                $mdDialog.show({
+        clickOutsideToClose: false,
+          template:
+            '   <md-dialog class="my-dialog">' +
+            '   <h3>Add task</h3>'+
+            '   <md-input-container class="my-container">'+
+            '   <label>Title</label>'+
+            '   <input type="text" ng-model="mod_item.title" ng-model-options="{ updateOn: \'blur\' }">'+
+            '   </md-input-container>'+
+            '   <md-input-container class="my-container">'+
+            '   <label>Description</label>'+
+            '   <input type="text" ng-model="mod_item.description" ng-model-options="{ updateOn: \'blur\' }">'+
+            '   </md-input-container>'+
+            '   <md-input-container  class="my-container">'+
+                '   <label>Priority</label>'+
+                 '<md-select name="type" ng-model="mod_item.priority">'+
+                '<md-option value="1">High</md-option>'+
+                '<md-option value="0">Normal</md-option>'+
+                '<md-option value="-1">Low</md-option>'+         
+                ' </md-select>'+
+                '   </md-input-container>'+
+            '   <md-input-container class="my-container">'+
+            '   <label>Tags</label>'+
+            '   <input type="text" ng-model="mod_item.tags" ng-model-options="{ updateOn: \'blur\' }">'+
+            '   </md-input-container>'+
+            '   <div class="my-container-datetime">'+
+            '   <div class="my-datetime" layout="row">'+
+            '   <md-input-container>'+
+            '   <label>Date</label>'+
+            '       <md-datepicker ng-model="mod_item.date"></md-datepicker>'+
+            '   </md-input-container>'+
+            '   <md-input-container>'+
+            '   <label>Time</label>'+
+            '   <input type="time" ng-model="mod_item.time"></input>'+
+            '   </md-input-container>'+
+            '   </div>'+
+            '   </div>'+
+            '   <md-input-container class="my-container">'+
+            '   <label>Estimated work in hours</label>'+
+            '   <input type="text" ng-model="mod_item.estimated_work" ng-model-options="{ updateOn: \'blur\' }">'+
+            '   </md-input-container>'+
+            '  <md-dialog-actions>' +
+            '    <md-button ng-click="closeDialog()" class="md-primary">' +
+            '      Close' +
+            '    </md-button>' +
+            '  </md-dialog-actions>' +
+            '</md-dialog>',
+          controller: function DialogController($scope, $mdDialog) {
+                $scope.mod_item=item;
+               
+                $scope.closeDialog = function() {
+                    storageService.set(vm.items);
+                    $mdDialog.hide();                 
+                }
+          }
+
+        });
+        }
+
     }
 })();
--- script.js
+++ script.js
@@ -6,11 +6,12 @@
     angular.module('todoApp').controller('TodoController', TodoController);
 
     //This is the application controller
-    function TodoController(storageService, $mdDialog) {
+    function TodoController(storageService, $mdDialog, $scope) {
         var vm = this;
 
-        vm.selectedItem = null;
+        vm.selectedItem = [];
         vm.items = storageService.get() || [];
+
 
         vm.notDone = function(item) {
             return item.done == false;
@@ -27,10 +28,16 @@
         //Delete the current selected item, if any
         vm.deleteItem = function(ev) {
 
-            if (vm.selectedItem != null) {
+            if (vm.selectedItem!=[]) {
+                var index=-1;
+                var i=0;
+                var msg;
+
+                if(vm.selectedItem.length==1) msg='The task ' +'"'+vm.selectedItem[i].title+'"';
+                else msg='The tasks selected ';
                 var confirm = $mdDialog.confirm()
 
-                .textContent('The task "' + vm.selectedItem.title + '" will be deleted. Are you sure?')
+                 .textContent('' + msg + ' will be deleted. Are you sure?')
                     .ariaLabel('Delete task')
                     .targetEvent(ev)
                     .ok('Yes')
@@ -38,44 +45,99 @@
 
                 $mdDialog.show(confirm).then(function(result) {
                     if (result) {
-                        var index = vm.items.indexOf(vm.selectedItem);
+                        while(i<vm.selectedItem.length){
+                        index = vm.items.indexOf(vm.selectedItem[i]);
                         if (index != -1) {
                             vm.items.splice(index, 1);
                             storageService.set(vm.items);
                         }
+                        i++;
+                    }
+                    vm.selectedItem = [];
                     }
                 });
             }
         }
 
         //Creates a new item with the given parameters
-        vm.createItem = function(title, priority, done, date) {
+        vm.createItem = function(title, description, priority, done, tags, date, time, estimated_work) {
             vm.items.push({
                 title: title,
-                done: done || false,
+                description: description,
                 priority: priority || 0,
-                date: date || Date.now()
+                done: done || false,
+                tags: tags,
+                date: date || Date.now(),
+                time: time || "00:00",
+                estimated_work: estimated_work
             });
             storageService.set(vm.items);
         }
 
-
-        //Add a new task to the items list 
-        vm.addTask = function(ev) {
-            var confirm = $mdDialog.prompt()
-                .title('Add new task')
-                .placeholder('Your task title...')
-                .ariaLabel('Your task title...')
-                .targetEvent(ev)
-                .ok('Add')
-                .cancel('Cancel');
-
-            $mdDialog.show(confirm).then(function(result) {
+    vm.addTask= function(ev){
+        vm.selectedItem = [];
+        $mdDialog.show({
+        clickOutsideToClose: true,
+          template:
+            '<md-dialog class="my-dialog">' +
+            '   <h3>Add task</h3>'+
+            '   <md-input-container class="my-container">'+
+            '   <label>Title</label>'+
+            '   <input type="text" ng-model="vm.title" required>'+
+            '   </md-input-container>'+
+            '   <md-input-container class="my-container">'+
+            '   <label>Description</label>'+
+            '   <input type="text" ng-model="vm.description">'+
+            '   </md-input-container>'+
+            '   <md-input-container  class="my-container">'+
+            '   <label>Priority</label>'+
+            '   <md-select name="type" ng-model="vm.priority">'+
+            '       <md-option value="1">High</md-option>'+
+            '       <md-option value="0">Normal</md-option>'+
+            '       <md-option value="-1">Low</md-option>'+
+            '   </md-select>'+
+            '   </md-input-container>'+
+            '   <md-input-container class="my-container">'+
+            '   <label>Tags</label>'+
+            '   <input type="text" ng-model="vm.tags">'+
+            '   </md-input-container>'+
+            '   <div class="my-container-datetime">'+
+            '   <div class="my-datetime" layout="row">'+
+            '   <md-input-container>'+
+            '   <label>Date</label>'+
+            '       <md-datepicker ng-model="vm.date"></md-datepicker>'+
+            '   </md-input-container>'+
+            '   <md-input-container>'+
+            '   <label>Time</label>'+
+            '   <input type="time" ng-model="vm.time"></input>'+
+            '   </md-input-container>'+
+            '   </div>'+
+            '   </div>'+
+            '   <md-input-container class="my-container">'+
+            '   <label>Estimated work in hours</label>'+
+            '   <input type="text" ng-model="vm.estimated_work">'+
+            '   </md-input-container>'+
+            '  <md-dialog-actions>' +
+            '    <md-button ng-click="addItem(vm)" class="md-primary">' +
+            '      Add' +
+            '    </md-button>' +
+            '    <md-button ng-click="closeDialog()" class="md-primary">' +
+            '      Close' +
+            '    </md-button>' +
+            '  </md-dialog-actions>' +
+            '</md-dialog>',
+          controller: function DialogController($scope, $mdDialog) {
+            $scope.closeDialog = function() {
+              $mdDialog.hide();
+            }
+            $scope.addItem = function (result) {
                 if (result)
-                    vm.createItem(result);
-            });
-        };
-
+                    vm.createItem(result.title, result.description, result.priority, result.done, result.tags, result.date, result.time, result.estimated_work);
+                $mdDialog.hide();
+          }
+          }
+        });
+    }
     }
 
 
